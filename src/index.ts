4b0c1da73dea08c590ccedc601e9a68a4d64c9ee
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-11-09 01:49:17 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const app = express()
 
 app.use(express.json())
 
-//cors
+app.use(cors());
 
 app.use("/", router)
 
