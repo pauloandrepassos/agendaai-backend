01ec488b26fa964b82daa33b5e998a0e206f2e54
eric-developer <ericspsantos@gmail.com>
2024-11-27 04:57:01 +0100

    atualização

--- src/database/config.ts
+++ src/database/config.ts
@@ -9,7 +9,7 @@ const AppDataSource = new DataSource({
   username: process.env.PG_USUARIO,
   password: process.env.PG_SENHA,
   database: process.env.PG_BANCO,
-  synchronize: true, //alterar para true para gerar a entidade
+  synchronize: false, //alterar para true para gerar a entidade
   logging: true,
   entities: [`${__dirname}/../models/*.{js,ts}`],
   migrations: [
