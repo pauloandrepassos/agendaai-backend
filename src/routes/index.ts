c622062329c3802ef0fa0d3f53db8e5a54c18270
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-11-28 09:04:52 -0300

    add rota de produtos no index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,10 @@ import authRouter from "./auth";
 import establishmentRouter from "./establishment";
 import establishmentRequestRouter from "./establishmentRequest";
 import operatingHoursRouter from "./operatingHours";
+import productRouter from "./product";
 
 const router = Router()
 
-router.use('/', userRouter, authRouter, establishmentRequestRouter, establishmentRouter,operatingHoursRouter)
+router.use('/', userRouter, authRouter, establishmentRequestRouter, establishmentRouter,operatingHoursRouter, productRouter)
 
 export default router
