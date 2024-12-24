df8f1cf4cf40a1828ee54015a8da87f6f2242164
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-12-24 14:00:48 -0300

    add categorias em produtos

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,12 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm";
 import { Establishment } from "./Establishment";
 
+export enum CategoryProduct {
+    SAVORY = "savoury",     //"salgado"
+    SWEET = "sweet",        //"doce"
+    CAKE = "cake",          //"bolo"
+    PIE = "pie",            //"torta"
+    DRINK = "drink",        //"bebida"
+    MEAL = "meal",          //"marmita"
+    BREAKFAST = "breakfast",//"café da manha"
+    OTHERS = "others"       // outros      
+}
+
 export interface IProduct {
     id: number;
     name: string;
     image: string
     description: string;
     price: number;
+    category: CategoryProduct
     establishment_id: number;
     created_at: Date;
     updated_at: Date;
@@ -28,6 +40,13 @@ export class Product implements IProduct {
 
     @Column("decimal", { precision: 10, scale: 2 })
     price: number;
+
+    @Column({
+        type: "enum",
+        enum: CategoryProduct,
+        default: CategoryProduct.OTHERS
+    })
+    category: CategoryProduct;
 
     @Column()
     establishment_id: number;
