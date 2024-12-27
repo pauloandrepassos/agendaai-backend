e43668ebb84b1f8e64382ac53deee43e2c25c51d
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-12-27 13:57:51 -0300

    adicionando formato de imagem permitida para fazer upload

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -11,9 +11,9 @@ import fs from 'fs'
 const upload = multer({
     dest: 'uploads/',
     fileFilter: (req, file, cb) => {
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
         if (!allowedTypes.includes(file.mimetype)) {
-            return cb(new Error('Tipo de arquivo inválido. Aceitamos apenas JPEG, PNG e JPG.'));
+            return cb(new Error('Tipo de arquivo inválido. Aceitamos apenas JPEG, PNG, WEBP e JPG.'));
         }
         cb(null, true);
     }
