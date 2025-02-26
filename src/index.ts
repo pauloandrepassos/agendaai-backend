e7979301eb7e7b85e1b7b09d5a48e3a685bb4cca
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2025-02-26 15:36:03 -0300

    editando cors

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,28 @@ import express from 'express';
 import router from './routes';
 import cors from 'cors';
 import timezoneMiddleware from './middlewares/timezone';
-import swaggerDocument from './swagger.json'
-import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json';
+import swaggerUi from 'swagger-ui-express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const corsOptions = {
     origin: ['https://agendaai.vercel.app', 'http://localhost:3000', 'https://agendeaii.com.br'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  };
-  
-  app.use(cors(corsOptions));
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 
-app.use(timezoneMiddleware)
+app.use(cors(corsOptions));
 
-app.use("/", router)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// Responde a todas as requisições OPTIONS
+app.options('*', cors(corsOptions));
 
-export default app
+app.use(timezoneMiddleware);
+
+app.use("/", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default app;
