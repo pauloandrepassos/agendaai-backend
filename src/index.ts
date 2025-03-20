91687cb00be568dc9d456c99879b40f412ec5fb8
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2025-03-20 14:50:35 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
-// Configuração do CORS
+app.use(express.json());
+
 const corsOptions = {
     origin: ['https://agendaai.vercel.app', 'http://localhost:3000', 'https://agendeaii.com.br'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -15,32 +16,14 @@ const corsOptions = {
     credentials: true,
 };
 
-// Aplica o CORS com as opções configuradas
 app.use(cors(corsOptions));
 
-// Middleware para responder a requisições OPTIONS (pré-flight)
+// Responde a todas as requisições OPTIONS
 app.options('*', cors(corsOptions));
 
-// Middleware para adicionar manualmente os cabeçalhos CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://agendeaii.com.br');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
-    next();
-});
-
-// Configuração para aumentar o limite de tamanho de upload
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-// Middleware para ajustar o fuso horário
 app.use(timezoneMiddleware);
 
-// Rotas da aplicação
 app.use("/", router);
-
-// Configuração do Swagger para documentação da API
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
